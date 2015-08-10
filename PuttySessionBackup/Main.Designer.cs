ea9be632ed8b152ee3e0e278413f9e94cb3be7d2
Edgars <eddmon999@gmail.com>
2015-08-10 18:03:20 +0300

    Initial commit

---
+++ PuttySessionBackup/Main.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace PuttySessionBackup
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.AllSessionsSelected = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Backup";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 244);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(437, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel1.Text = "Status:";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Step = 1;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(6, 19);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 124);
+            this.checkedListBox1.TabIndex = 3;
+            // 
+            // AllSessionsSelected
+            // 
+            this.AllSessionsSelected.AutoSize = true;
+            this.AllSessionsSelected.Location = new System.Drawing.Point(6, 149);
+            this.AllSessionsSelected.Name = "AllSessionsSelected";
+            this.AllSessionsSelected.Size = new System.Drawing.Size(112, 17);
+            this.AllSessionsSelected.TabIndex = 4;
+            this.AllSessionsSelected.Text = "Select all sessions";
+            this.AllSessionsSelected.UseVisualStyleBackColor = true;
+            this.AllSessionsSelected.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkedListBox1);
+            this.groupBox1.Controls.Add(this.AllSessionsSelected);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(145, 179);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sessions:";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 266);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button1);
+            this.Name = "Main";
+            this.Text = "Putty session backup";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckBox AllSessionsSelected;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ PuttySessionBackup/Main.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace PuttySessionBackup
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            //Fill in 
+            FillCheckedListBox();
+        }
+
+        string FullKeyPath = @"HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions";
+
+        RegistryKey sessionsKey = Registry.CurrentUser.OpenSubKey(@"Software\SimonTatham\PuTTY\Sessions");
+
+        private void FillCheckedListBox()
+        {
+            try
+            {
+                var sessionNames = sessionsKey.GetSubKeyNames();
+                if (sessionNames.Count() > 0)
+                {
+                    foreach (var sessionName in sessionNames)
+                    {
+                        checkedListBox1.Items.Add(sessionName);
+                    }
+                }
+
+                toolStripStatusLabel2.Text = "Ready";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (checkedListBox1.CheckedItems.Count > 0)
+                {
+                    
+                    var result = saveFileDialog1.ShowDialog();
+                    
+
+
+                    if (string.IsNullOrEmpty(saveFileDialog1.FileName))
+                        throw new ArgumentNullException("Target file path not set!");
+
+                    try
+                    {
+                        string path = saveFileDialog1.FileName;
+                        string key = FullKeyPath;
+
+                        ProcessStartInfo startInfo = new ProcessStartInfo();
+                        startInfo.FileName = "regedit.exe";
+                        startInfo.CreateNoWindow = true;
+                        startInfo.UseShellExecute = false;
+                        
+                        startInfo.Arguments = "/e " + path + " " + key + "";
+
+                        using (Process proc = Process.Start(startInfo))
+                        {
+                            proc.WaitForExit();
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        throw ex;
+                    }
+                }else
+                {
+                    throw new NotImplementedException();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(AllSessionsSelected.Checked)
+            {
+                //Disable checking
+                checkedListBox1.Enabled = false;
+
+                //Select all items
+                for (int itemIndex = 0; itemIndex < checkedListBox1.Items.Count; itemIndex++)
+                {
+                    checkedListBox1.SetItemChecked(itemIndex, true);
+                }
+            }
+            else
+            {
+                checkedListBox1.Enabled = true;
+
+                for (int itemIndex = 0; itemIndex < checkedListBox1.Items.Count; itemIndex++)
+                {
+                    checkedListBox1.SetItemChecked(itemIndex, false);
+                }
+            }
+        }
+    }
+}
